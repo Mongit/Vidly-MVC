95403fcfc5bb16d733a0da47ecf687ed1b5e4cce
Monse <monserrat.jimenez2@gmail.com>
2022-08-10 15:37:55 -0500

    Adding Validation and custom validation

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -34,6 +34,17 @@ namespace Vidly2.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)//also binds Customer because the fields of the form have .Customer. as prefix, and all attribute names are from Customers
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
+
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
