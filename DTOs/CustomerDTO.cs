28a232eee4b87a4e2d5cea28bc38ade267be91d7
Monse <monserrat.jimenez2@gmail.com>
2022-08-11 13:22:08 -0500

    Creating DTOs

---
+++ DTOs/CustomerDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Vidly2.Models;
+
+namespace Vidly2.DTOs
+{
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
