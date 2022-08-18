0e397cca1216a000347c2946b196354079f67c88
Monse <monserrat.jimenez2@gmail.com>
2022-08-18 09:41:20 -0500

    Exercise 8 - Missing fields

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -14,6 +14,7 @@ namespace Vidly2.Models
         public string DrivingLicense { get; set; }
 
         [Required]
+        [StringLength(50)]
         public string Phone { get; set; }
     }
 
@@ -76,6 +77,7 @@ namespace Vidly2.Models
         public string DrivingLicense { get; set; }
 
         [Required]
+        [StringLength(50)]
         public string Phone { get; set; }
 
         [Required]
